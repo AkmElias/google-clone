0876768656acac4efb8b2f5b6046006e981a2765
AkmElias <akmelias11@gmail.com>
2020-08-22 19:50:39 +0600

    one time push with finishing touch > > -- google home page UI design > -- search component > -- search result page UI design > -- google custom search with google custom api > -- google result maintain with react context api

---
+++ src/UseGoogleSearch.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import API_KEY from "./keyes";
+
+const CONTEXT_KEY = "239d4b7462e73fb67";
+
+const UseGoogleSearch = (term) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setData(result);
+        });
+    };
+    fetchData();
+  }, [term]);
+
+  return { data };
+};
+
+export default UseGoogleSearch;
---
+++ src/keyes.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyCkZjmN3XO9vULtUwPDqdPazVS_eHJBJ3g";
+
+export default API_KEY;
